c4b66f9107cff3128bfde7b0fbcecfd9643a8918
John Foley <jfoley@pivotal.io>
2017-07-05 17:13:10 -0600

    solution with tests for Spring Boot App

---
+++ src/test/java/io/pivotal/pal/trackertest/PalTrackerTest.java
@@ -0,0 +1,27 @@
+package io.pivotal.pal.trackertest;
+
+import io.pivotal.pal.tracker.PalTrackerApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+public class PalTrackerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void exampleTest() {
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertThat(body).isEqualTo("hello");
+    }
+
+}
---
+++ src/test/java/io/pivotal/pal/trackertest/TrackerControllerTest.java
@@ -0,0 +1,16 @@
+package io.pivotal.pal.trackertest;
+
+import io.pivotal.pal.tracker.TrackerController;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TrackerControllerTest {
+    @Test
+    public void itSaysHello() throws Exception {
+        TrackerController controller = new TrackerController();
+
+        assertThat(controller.sayHello()).isEqualTo("hello");
+    }
+
+}
