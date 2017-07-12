bcc1f1fbf92b22f4f861f3de23064830be24d3bb
Shane Lattanzio <pair+slattanzio@pivotal.io>
2017-07-12 11:11:36 -0400

    Fun stuff

--- src/main/java/io/pivotal/pal/tracker/SecurityConfiguration.java
+++ src/main/java/io/pivotal/pal/tracker/SecurityConfiguration.java
@@ -20,10 +20,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         }
 
         http.authorizeRequests().antMatchers("/**").hasRole("USER").and().httpBasic().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().httpBasic().and().csrf().disable();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("shane").password("woot").roles("ADMIN");
     }
 }
