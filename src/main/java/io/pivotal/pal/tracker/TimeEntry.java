90c773c3dc17b0bac355e1c4f29377ce92b7a316
Shane Lattanzio <pair+slattanzio@pivotal.io>
2017-07-18 14:47:43 -0400

    Add lombok

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -1,17 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by pivotal on 2017-07-11.
  */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TimeEntry {
     private long id;
     private long projectId;
     private long userId;
     private String date;
     private int hours;
-
-    public TimeEntry() {
-    }
 
     public TimeEntry(long projectId, long userId, String date, int hours) {
         this.projectId = projectId;
@@ -20,65 +26,4 @@ public class TimeEntry {
         this.hours = hours;
     }
 
-    public TimeEntry(long id, long projectId, long userId, String date, int hours) {
-        this.id = id;
-        this.projectId = projectId;
-        this.userId = userId;
-        this.date = date;
-        this.hours = hours;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id){
-        this.id = id;
-    }
-
-    public long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(long projectId){
-        this.projectId = projectId;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId){
-        this.userId = userId;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date){
-        this.date = date;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours){
-        this.hours = hours;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        TimeEntry timeEntry = (TimeEntry) o;
-
-        if (id != timeEntry.id) return false;
-        if (projectId != timeEntry.projectId) return false;
-        if (userId != timeEntry.userId) return false;
-        if (hours != timeEntry.hours) return false;
-        return date != null ? date.equals(timeEntry.date) : timeEntry.date == null;
-    }
 }
